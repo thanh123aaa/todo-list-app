d54b9cd8a02d8d588b2fc87bfa8aecfc11ec9fe7
bichtram1402 <thanhpham.a2.2004@gmail.com>
2026-07-05 20:18:46 +0700

    Allow all CORS origin patterns

--- backend/src/main/java/com/example/todo/config/WebConfig.java
+++ backend/src/main/java/com/example/todo/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:80", "http://localhost")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
